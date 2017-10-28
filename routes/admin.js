46169a7fc26d15bb52653acd95bc216749de4059
Phantom <kenichixie@gmail.com>
2017-10-28 12:02:31 +0700

    change user to unique

--- routes/admin.js
+++ routes/admin.js
@@ -10,4 +10,45 @@ router.get('/', middleware.isLoggedIn, (req, res) => {
   res.render('admin')
 })
 
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// handle sign up logic
+router.post('/register', (req, res) => {
+  const newUser = new User({username: req.body.username})
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err)
+      return res.render('register')
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/')
+    })
+  })
+})
+
+// show login form
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// handling login logic
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}), (req, res) => {
+})
+
+// logout route
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -7,45 +7,4 @@ router.get('/', function (req, res) {
   res.render('main')
 })
 
-//  ===========
-// AUTH ROUTES
-//  ===========
-
-// show register form
-router.get('/register', (req, res) => {
-  res.render('register')
-})
-
-// handle sign up logic
-router.post('/register', (req, res) => {
-  const newUser = new User({username: req.body.username})
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      console.log(err)
-      return res.render('register')
-    }
-    passport.authenticate('local')(req, res, () => {
-      res.redirect('/')
-    })
-  })
-})
-
-// show login form
-router.get('/login', (req, res) => {
-  res.render('login')
-})
-
-// handling login logic
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-}), (req, res) => {
-})
-
-// logout route
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
-
 module.exports = router
