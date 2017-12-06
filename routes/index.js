3dbe1d4c3c4f4c3fcf51495df6f337acc08c54ef
HenryTanjaya <henrytanjaya11@gmail.com>
2017-12-06 18:31:39 +0700

    mail

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const Event = require('../models/event')
-var nodemailer = require('nodemailer')
-var GMAIL_PASSWORD = process.env.GMAIL_PASSWORD || 'password'
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/event');
+var nodemailer = require('nodemailer');
+var GMAIL_PASSWORD = process.env.GMAIL_PASSWORD || 'Alderbeagle2017'
 
 router.get('/', (req, res) => {
   Event.find().sort({dateStart: -1}).limit(1).exec(function (err, event) {
@@ -23,23 +23,27 @@ router.post('/', function (req, res) {
     }
   })
 
-  var mailOptions = {
-    from: 'bemuphmedan@ardent-family.com',
-    to: 'henrytanjaya11@gmail.com',
-    subject: 'Form',
-    html: '<b>Name : </b>' + req.body.name +
-            '<br><b>Email : </b>' + req.body.email +
-            '<br><b>Message : </b>' + req.body.message
-  }
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log('Email sent: ' + info.response)
-      res.redirect('/')
+  if (!req.body.name || !req.body.email || !req.body.message) {
+    res.redirect('/')
+  } else {
+    var mailOptions = {
+      from: 'bemuphmedan@gmail.com',
+      to: 'henrytanjaya11@gmail.com',
+      subject: 'Form',
+      html: '<b>Name : </b>' + req.body.name +
+        '<br><b>Email : </b>' + req.body.email +
+        '<br><b>Message : </b>' + req.body.message
     }
-  })
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+        res.redirect('/')
+      }
+    })
+  }
 })
 
 module.exports = router
